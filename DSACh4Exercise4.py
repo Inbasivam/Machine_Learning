cd8147a970edbb9bfc6aa80a60507f350f73196e
Inbasivam P <184803082+Inbasivam@users.noreply.github.com>
2026-06-10 20:02:40 +0530

    Delete DSACh4Exercise4.py

--- DSACh4Exercise4.py
+++
@@ -1,9 +0,0 @@
-def gratest_number(array):
-    if not array:
-        return None
-    greatest = 0
-    for num in array:
-        if num > greatest:
-            greatest = num
-    return greatest
-print(gratest_number([10,50,1,55,78]))
